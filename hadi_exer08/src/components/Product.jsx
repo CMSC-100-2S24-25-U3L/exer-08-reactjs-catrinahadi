8a4b71bde3f15df940e3741cb4ab08479f6f076b
catrinahadi <160173804+catrinahadi@users.noreply.github.com>
2025-04-02 12:05:21 +0800

    added comments

--- hadi_exer08/src/components/Product.jsx
+++ hadi_exer08/src/components/Product.jsx
@@ -1,16 +1,16 @@
 function Product({ products }) {
     return (
         <div className="products">
-            {products.map((product) => ( //map function to display all the items in the products
+            {products.map((product) => ( 
                 <div className="product-card" key={product.id}>
-                    <img className="product-image" src={product.image}/>
-                    <h1 className="product-name">{product.name}</h1>
-                    <p className="product-price">${product.price}</p>
-                    <button className="add-to-cart">Add to Cart</button>
+                    <img className="product-image" src={product.image} alt={product.name} /> // display product image
+                    <h1 className="product-name">{product.name}</h1> // display product name
+                    <p className="product-price">${product.price}</p> // display product price
+                    <button className="add-to-cart">Add to Cart</button> // button to add product to cart
                 </div>
             ))}
         </div>
     );
 }
 
-export default Product
+export default Product;
